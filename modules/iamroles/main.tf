c587caa4b8431b80fb017f69f871afecc7016c90
tech1master <tech1@LAPTOP-SVS1G8EO.localdomain>
2025-11-29 22:08:32 +0800

    update

--- modules/iamroles/main.tf
+++ modules/iamroles/main.tf
@@ -26,7 +26,6 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-
 # ECS TASK ROLE
 resource "aws_iam_role" "ecs_task_role" {
   name_prefix        = "${var.environment}-ecs-task"
@@ -49,4 +48,33 @@ resource "aws_iam_role" "ecs_task_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
   role       = aws_iam_role.ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+# EC2
+resource "aws_iam_role" "ssm_role" {
+  name_prefix        = "${var.environment}-ssm-ec2"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore" 
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "${var.environment}-ssm-ec2-profile"
+  role = aws_iam_role.ssm_role.name
 }
